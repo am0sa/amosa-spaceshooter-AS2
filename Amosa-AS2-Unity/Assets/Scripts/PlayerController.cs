e612612b7a6bb952bebecdc5ae9605498af2ef22
Amosa <amosa.o94@gmail.com>
2019-10-25 21:57:53 -0400

    FridayNight
    Shot power level,
    Player abilities

--- Amosa-AS2-Unity/Assets/Scripts/PlayerController.cs
+++ Amosa-AS2-Unity/Assets/Scripts/PlayerController.cs
@@ -13,8 +13,18 @@ public class PlayerController : MonoBehaviour
     public Animator animator;
 
     public int scoreEarned;
+    
+    public float blinkRecharge;
 
-    public enum powerLevel
+    public float RECHARGE_TIME;
+
+    public bool blinkIsCharging;
+
+    public bool cannonCharge;
+
+    public float cannonChargeTimer;
+
+    public enum PowerLevel
     {
         one,
         two,
@@ -26,11 +36,24 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         playerSpeed = 1f;
+        cannonChargeTimer = 0f;
+        RECHARGE_TIME = 3f;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        if (blinkIsCharging)
+        {
+            blinkRecharge -= Time.deltaTime;
+
+            if(blinkRecharge <= 0)
+            {
+                blinkIsCharging = false;
+            } 
+        }
+
+
         var movementDir = new Vector2(0, 0);
 
         if (Input.GetAxis("Horizontal") > 0)
@@ -67,7 +90,84 @@ public class PlayerController : MonoBehaviour
             //no change to movementDir
         }
 
+        //------!!!!!!!------movementDir value set------!!!!!!------
         movementDir = (movementDir.normalized * playerSpeed * Time.deltaTime);
+
+        //------!!!!!!!------movementDir value use------!!!!!!------
         transform.position += (Vector3)movementDir;
+
+
+        if (Input.GetKeyDown(KeyCode.LeftShift) && !blinkIsCharging)
+        {
+            blinkIsCharging = Blink(movementDir);
+            blinkRecharge = RECHARGE_TIME;
+        }
+
+        if (Input.GetKey(KeyCode.RightShift))
+        {
+            cannonChargeTimer += Time.deltaTime;
+        }
+        else if (Input.GetKeyUp(KeyCode.RightShift))
+        {
+            int pow = (int)cannonChargeTimer;
+            Shoot(pow);
+        }
+
+
+
+        //------!!!!!!-------free space continues
     }
+
+    public bool Blink(Vector2 movementDir)
+    {
+        Vector2 destination = playerSpeed * ((Vector2)transform.position + movementDir);
+        if ((Vector2)transform.position == destination)
+        {
+            return false;
+        }
+        else
+        {
+            transform.position = (Vector3)destination;
+            return true;
+        }
+    }
+
+    public void Shoot()
+    {
+        //Instantiate rapidfire bullets
+    }
+
+    public void Shoot(int pow)
+    {
+        PowerLevel shotPower = PowerLevel.one;
+        if (pow >= 3)
+        {
+            shotPower = PowerLevel.super;
+        }
+        else if (pow >=2 )
+        {
+            
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
